a844bd2cd3167cdb6a5d5d1504147573b8dd6797
naishadhingra16 <85798566+naishadhingra16@users.noreply.github.com>
2021-06-12 21:00:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+  //spookySound.loop();
+ 
+}
+
+function draw(){
+  background(0);
+
+  drawSprites();
+}
+
